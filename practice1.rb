a9cfba641a9d6c451428af4468a2989795b190bf
Daiki Watanabe <6.beth763trrr.8@gmail.com>
2019-05-15 15:17:31 +0900

    年齢計算処理を修正

--- practice1.rb
+++ practice1.rb
@@ -2,15 +2,10 @@ require 'csv'
 require 'date'
 
 def calc_age(birthday, reference_date)
-  age = reference_date.year - birthday.year
-  if birthday.month > reference_date.month ||
-    (birthday.month == reference_date.month && birthday.day > reference_date.day)
-    # 誕生日を迎えていない場合
-    age -= 1
-  else
-    # 誕生日を迎えている場合
-    age
+  if birthday !~ /^[0-9]{8}$/
+    return -1
   end
+  return (reference_date.to_i - birthday.to_i) / 10000
 end
 
 csv_data = CSV.read('dummy.csv', headers: true)
@@ -20,12 +15,14 @@ CSV.open('output.csv', 'w') do |file|
   file << header
 
   csv_data.each do |data|
-    birthday = Date.parse(data['誕生日'])
-    reference_date = Date.new(2020, 7, 24)
+    birthday_year = data['誕生日'].split('/')[0]
+    birthday_month = data['誕生日'].split('/')[1].rjust(2, '0')
+    birthday_day = data['誕生日'].split('/')[2].rjust(2, '0')
+    birthday = birthday_year + birthday_month + birthday_day
+    reference_date = '20200724'
     age = calc_age(birthday, reference_date)
 
     if (30..40).include?(age) && data['性別'] == '女'
-      # 2020年7月24日時点で30歳〜40歳の女性を抽出
       file << [data['名前'], data['誕生日'], age]
     end
   end
