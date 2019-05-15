eee45e2a0de75c3d7b4c9a894130709bef2fab73
Daiki Watanabe <6.beth763trrr.8@gmail.com>
2019-05-15 16:55:26 +0900

    不要なコメントの削除 誕生日のyyyy/m/d→yyyymmdd変換部分の修正

--- practice1.rb
+++ practice1.rb
@@ -15,34 +15,13 @@ CSV.open('output.csv', 'w') do |file|
   file << header
 
   csv_data.each do |data|
-    birthday_year = data['誕生日'].split('/')[0]
-    birthday_month = data['誕生日'].split('/')[1].rjust(2, '0')
-    birthday_day = data['誕生日'].split('/')[2].rjust(2, '0')
-    birthday = birthday_year + birthday_month + birthday_day
+    birthday = Date.new(data['誕生日']).strftime('yyyymmdd')
     reference_date = '20200724'
     age = calc_age(birthday, reference_date)
 
     if (30..40).include?(age) && data['性別'] == '女'
+      # 2020年7月24日時点で30〜40際の女性を抽出
       file << [data['名前'], data['誕生日'], age]
     end
   end
 end
-
-# csv_data.each do |data|
-#   birthday = Date.parse(data['誕生日'])
-#   reference_date = Date.new(2020, 7, 24)
-#   age = Birthday.calc_age(birthday, reference_date)
-
-#   if 30 <= age && age <= 40 && data['性別'] == '女'
-#     output_data = [data['名前'], data['誕生日'], age]
-#     output_data_list << output_data
-#   end
-# end
-
-# header = ['名前', '誕生日', 'オリンピック開催日']
-# CSV.open('output.csv', 'w') do |file|
-#   file << header
-#   output_data_list.each do |data|
-#     file << data
-#   end
-# end
