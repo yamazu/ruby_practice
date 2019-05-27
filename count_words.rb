a87b1072ea71b2d3ff577460b5287bfb30c42e77
yamazu <6.beth763trrr.8@gmail.com>
2019-05-27 14:28:47 +0900

    Rubyで英語記事に含まれてる英単語を数えて出現数順にソートする」問題解答追加

--- count_words.rb
+++ count_words.rb
@@ -1,29 +1,40 @@
 class CountWords
   def self.count
+    self.new.count
+  end
+
+  def count
     file_path = File.expand_path('../resource/input.txt', __FILE__)
     text = File.read(file_path)
-    hash_idioms = Hash.new(0)
-    hash_words = Hash.new(0)
+    count_table = Hash.new(0)
 
-    total_number_of_words = 0
-    text.gsub(/\w+/) do |word|
-      hash_words[word] += 1
-      total_number_of_words += 1
+    word_char = '[\w’\/-]'
+    regex_idioms = /[A-Z]#{word_char}*(?: of| [A-Z]#{word_char}*)+/
+    regex_words = /#{word_char}+/
+    regex = Regexp.union(regex_idioms, regex_words)
+    # 英熟語・英単語の出現回数をカウント
+    text.gsub(regex) { |word| count_table[word] += 1 }
+
+    # 英熟語と英単語のハッシュに分離
+    count_table_idioms, count_table_words = count_table.partition { |word, _| word.include?(' ') }
+
+    # 結果出力
+    total_word_count = count_table_words.inject(0) { |sum, (_, count)| sum + count }
+    puts "単語数（熟語以外）：#{total_word_count}"
+    output_result(count_table_idioms, '英熟語？')
+    output_result(count_table_words, '英単語')
+  end
+
+  private
+
+  def output_result(count_table, header)
+    puts "#{header}------------------------------------------------------------------"
+    count_table_group_by_count = Hash.new { [] }
+    count_table.each do |word, count|
+      count_table_group_by_count[count] = count_table_group_by_count[count].push(word)
     end
-    puts "単語数（熟語以外）：#{total_number_of_words}"
-    puts "英単語------------------------------------------------------------------"
-    # hash_words
-    #   .sort_by { |word, count| -count }
-    #   .to_h
-    #   .each { |word, count| puts "#{count.to_s.rjust(3)} #{word}" }
-    hash_words_group_by_count = Hash.new { [] }
-    hash_words.each do |word, count|
-      hash_words_group_by_count[count] = hash_words_group_by_count[count].push(word)
-    end
-    hash_words_group_by_count.sort.reverse.to_h.each do |count, words|
-      words.sort.each { |word| puts "#{count.to_s.rjust(3)} #{word}" }
+    count_table_group_by_count.sort.reverse.to_h.each do |count, words|
+      words.sort_by(&:downcase).each { |word| puts "#{count.to_s.rjust(3)} #{word}" }
     end
   end
 end
-
-CountWords.count
--- count_words_test.rb
+++ count_words_test.rb
@@ -3,17 +3,13 @@ require_relative './count_words'
 
 class CountWordsTest < Minitest::Test
   def test_count_words
-    assert_equal expected_text, CountWords.count(input_file_path)
+    assert_output(expected_text) { CountWords.count }
   end
 
   private
 
-  def input_file_path
-    File.expand_path('../resource/input.txt', __FILE__)
-  end
-
   def expected_text
     file_path = File.expand_path('../expected_output.txt', __FILE__)
-    File.read(file_path).strip
+    File.read(file_path)
   end
 end
